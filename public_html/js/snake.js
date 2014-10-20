45b6be708939765f91c017fd69e1c318ba208f4c
Anna Simon <anna@rocknrollrobots25.com>
2014-10-20 10:44:20 -0700

    added a lot did a lot of stuff

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,45 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
+var snakeLength;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
 
 
+gameInitialize();
+gameDraw();
+
+function gameInitialize(){
+   var canvas=document.getElementById("game-screen");
+   context = canvas.getContext("2d");
+   
+   screenWidth = window.innerWidth;
+   screenHeight = window.innerHeight;
+   
+   canvas.width = screenWidth;
+   canvas.height = screenHeight;
+}
+
+function gameLoop(){
+    
+}
+
+function gameDraw(){
+    context.fillStyle = "rgb(160,232,165)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize(){
+    snake= [];
+    snakeLength = 15;
+    snakeSize = 20;
+}
+
+function snakeDraw(){
+    
+}
+
+function snakeUpdate(){
+    
+}
